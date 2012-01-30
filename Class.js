9e798d2df19b8de557faa61464e3941a5826c761
Ryan Sullivan <ryan@oroboto.com>
2012-01-30 15:58:21 -0800

    refactor for compression optimization

--- Class.js
+++ Class.js
@@ -1,70 +1,56 @@
 /*
+ * Class: Super lightweight class-based inheritance for JavaScript
+ * http://oroboto.github.com/class/
+ *
  * Copyright (c) 2012 Oroboto. All rights reserved.
- *
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
+ * MIT License: http://oroboto.github.com/class/LICENSE
  */
 (function (namespace) {
     // http://caniuse.com/use-strict
     // http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more/
     "use strict";
 
-    namespace.Class = namespace.Class || {
-        create: function (members) {
-            if (!members) { return; }
+    // string constants to optimize compression
+    var CTOR = 'constructor',
+        PROTO = 'prototype';
 
-            var base = members.base || Object, // all prototype chains lead to Object
-                baseCtor = base.prototype.constructor,
-                ctor = members.hasOwnProperty('constructor') ? members.constructor : baseCtor,
-                proto = members.prototype,
+    namespace.Class = namespace.Class || {};
+    namespace.Class.create = function (members) {
+        if (!members) { return; }
 
-                // subclass constructor
-                Class = function (child, parent) {
-                    // inherit base properties
-                    var key;
-                    for (key in parent) {
-                        if (parent.hasOwnProperty(key)) {
-                            child[key] = parent[key];
-                        }
+        var base = members.base || Object, // all prototype chains lead to Object
+            baseCtor = base[PROTO][CTOR],
+            // try to use defined constructor, falling back to base constructor
+            ctor = members.hasOwnProperty(CTOR) ? members[CTOR] : baseCtor,
+            proto = members[PROTO],
+            subclass = function (child, parent) {
+                var key;
+
+                // inherit base properties
+                for (key in parent) {
+                    if (parent.hasOwnProperty(key)) {
+                        child[key] = parent[key];
                     }
+                }
 
-                    // inherit prototype
-                    function Class() {} // constructor to host base prototype
-                    Class.prototype = child.__base__ = parent.prototype;
-                    child.prototype = new Class;
-                    child.prototype.constructor = child;
-                    return child;
-                }(function () { // pass modified constructor as argument to subclass function
-                    // add base constructor to object so subclass can call it
-                    this._base = baseCtor;
+                // inherit prototype
+                function Class () {} // constructor to host base prototype
+                Class[PROTO] = child.__base__ = parent[PROTO];
+                child[PROTO] = new Class;
+                child[PROTO][CTOR] = child;
+                return child;
+            }(function () { // pass modified constructor as argument to subclass function
+                // add base constructor to object so subclass can call it
+                this._base = baseCtor;
 
-                    // execute original constructor
-                    ctor.apply(this, arguments);
-                }, base);
+                // execute original constructor
+                ctor.apply(this, arguments);
+            }, base);
 
-            // add members to prototype
-            if (proto) { proto.call(Class.prototype, Class.__base__); }
+        // add members to prototype
+        if (proto) { proto.call(subclass[PROTO], subclass.__base__); }
 
-            return Class;
-        }
+        return subclass;
     };
 
 })(this); // change argument to namespace of your choice; default is "this" (global or window)
